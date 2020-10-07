8757e82359d7c886525be675cd02d6687572190f
1-n-only-nobody <16bce100@nirmauni.ac.in>
2020-10-07 23:04:33 +0530

    corrected create table query

--- Contacts_Networking_Application/src/com/training/utils/ConnectionUtility.java
+++ Contacts_Networking_Application/src/com/training/utils/ConnectionUtility.java
@@ -17,7 +17,7 @@ public class ConnectionUtility {
 		
 		Connection derbyConnection=null;
 			
-//		create table userTable ( userid INT not null primary key GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), username varchar(255) unique not null, password varchar(255) not null, fullName varchar(255) not null, email varchar(255) unique not null, phone bigint unique, gender varchar(255), dateOfBirth date, address varchar(255), city varchar(255), state varchar(255), country varchar(255), company varchar(255), image blob);
+//		create table userTable ( userid INT not null primary key GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), username varchar(255) unique not null, password varchar(255) not null, fullName varchar(255) not null, email varchar(255) unique not null, phone bigint unique, gender varchar(255), dateOfBirth date, address varchar(255), city varchar(255), state varchar(255), country varchar(255), company varchar(255), image blob, isdisabled smallint, isdeleted smallint);
 		
 		String url = "jdbc:derby:D:\\Database\\CodeFuryDB;create=true";
         String user = "admin";
